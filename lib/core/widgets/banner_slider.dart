5ff10a21d2ec211e7d189654cde36fb9c215a3e3
lahcenegue <lahcene90.lg@gmail.com>
2023-10-21 12:29:19 +0100

    update banner slider

--- lib/core/widgets/banner_slider.dart
+++ lib/core/widgets/banner_slider.dart
@@ -1,42 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
-//TODO stateless
-
-class BannerSlider extends StatefulWidget {
+class BannerSlider extends StatelessWidget {
   final List urlImages;
   final double height;
-  const BannerSlider(
-      {super.key, required this.urlImages, required this.height});
+  const BannerSlider({
+    super.key,
+    required this.urlImages,
+    required this.height,
+  });
 
-  @override
-  State<BannerSlider> createState() => _BannerSliderState();
-}
-
-class _BannerSliderState extends State<BannerSlider> {
-  final controller = CarouselController();
-
-  int activeIndex = 0;
+  //final controller = CarouselController();
+  //int activeIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         CarouselSlider.builder(
-          itemCount: widget.urlImages.length,
+          itemCount: urlImages.length,
           itemBuilder: (context, index, realindex) {
-            final urlImage = widget.urlImages[index];
+            final urlImage = urlImages[index];
             return buildImage(urlImage, index);
           },
           options: CarouselOptions(
             aspectRatio: 16 / 9,
             viewportFraction: 1,
-            height: widget.height,
+            height: height,
             autoPlay: false,
             enableInfiniteScroll: false,
             autoPlayAnimationDuration: const Duration(seconds: 2),
             enlargeCenterPage: true,
-            onPageChanged: (index, reason) =>
-                setState(() => activeIndex = index),
+            //onPageChanged: (index, reason) {},
           ),
         ),
       ],
